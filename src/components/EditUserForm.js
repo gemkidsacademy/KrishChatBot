48118ffc5355cc122df2f41906aa69b3030366a9
Sajjad Ali Noor <proactive1.san@gmail.com>
2025-10-20 11:41:22 +0500

    Update EditUserForm.js

--- src/components/EditUserForm.js
+++ src/components/EditUserForm.js
@@ -33,7 +33,7 @@ function EditUserForm({ onClose, onUserUpdated }) {
 
     const fetchUserDetails = async () => {
       try {
-        const res = await fetch(`https://krishbackend-production.up.railway.app/user/${selectedUserId}`);
+        const res = await fetch(`https://krishbackend-production.up.railway.app/get-user/${selectedUserId}`);
         if (!res.ok) throw new Error("Failed to fetch user details");
         const user = await res.json();
         setName(user.name || "");
